c77388fbcc934608e83dd17bc6c5b1497b06ef7e
Wookyung <namwookyung.dev@gmail.com>
2023-06-07 08:17:00 +0900

    게시글에 들어갈 정보 자바 파일 생성

---
+++ WebBoard/src/main/java/wb/Wb.java
@@ -0,0 +1,50 @@
+package wb;
+
+public class Wb {
+	
+	private int wbID;
+	private String wbTitle;
+	private String userID;
+	private String wbDate;
+	private String wbContent;
+	private int wbAvailable;
+	
+	
+	public int getWbID() {
+		return wbID;
+	}
+	public void setWbID(int wbID) {
+		this.wbID = wbID;
+	}
+	public String getWbTitle() {
+		return wbTitle;
+	}
+	public void setWbTitle(String wbTitle) {
+		this.wbTitle = wbTitle;
+	}
+	public String getUserID() {
+		return userID;
+	}
+	public void setUserID(String userID) {
+		this.userID = userID;
+	}
+	public String getWbDate() {
+		return wbDate;
+	}
+	public void setWbDate(String wbDate) {
+		this.wbDate = wbDate;
+	}
+	public String getWbContent() {
+		return wbContent;
+	}
+	public void setWbContent(String wbContent) {
+		this.wbContent = wbContent;
+	}
+	public int getWbAvailable() {
+		return wbAvailable;
+	}
+	public void setWbAvailable(int wbAvailable) {
+		this.wbAvailable = wbAvailable;
+	}
+	
+}
